37dae38865183fee995f506cdfb6f634f70b705c
Erik Hakamada <erik.hakamada@hotmail.com>
2023-10-22 22:04:16 -0300

    feat: spliting responsability and add gitignore

--- infra/main.tf
+++ infra/main.tf
@@ -1,66 +1,72 @@
-provider "aws" {
-  region = "sa-east-1" # Modifique conforme sua região
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "eks-vpc"
-  }
+provider "aws" {
+  profile = "default"
+  region = var.region
 }
 
 resource "aws_subnet" "eks_subnet_public_1" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "sa-east-1" # Modifique conforme sua região e zona de disponibilidade
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = var.region # Modifique conforme sua região e zona de disponibilidade
   map_public_ip_on_launch = true
-  tags = {
+  tags                    = {
     Name = "eks-subnet-public-1"
   }
 }
 
-resource "aws_subnet" "eks_subnet_public_2" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "sa-east-1" # Modifique conforme sua região e zona de disponibilidade
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "eks-subnet-public-2"
-  }
-}
-
 resource "aws_subnet" "eks_subnet_private_1" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "sa-east-1" # Modifique conforme sua região e zona de disponibilidade
-  tags = {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = var.region # Modifique conforme sua região e zona de disponibilidade
+  tags              = {
     Name = "eks-subnet-private-1"
   }
 }
 
+resource "aws_subnet" "eks_subnet_public_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = var.region # Modifique conforme sua região e zona de disponibilidade
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name = "eks-subnet-public-2"
+  }
+}
+
 resource "aws_subnet" "eks_subnet_private_2" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.4.0/24"
-  availability_zone = "sa-east-1" # Modifique conforme sua região e zona de disponibilidade
-  tags = {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = var.region # Modifique conforme sua região e zona de disponibilidade
+  tags              = {
     Name = "eks-subnet-private-2"
   }
 }
 
 resource "aws_security_group" "eks_default_sg" {
   vpc_id = aws_vpc.eks_vpc.id
-  tags = {
+  tags   = {
     Name = "eks-default-sg"
   }
 }
 
-resource "aws_internet_gateway" "eks_igw" {
+resource "aws_internet_gateway" "eks_igw" { #Internet Gateway
   vpc_id = aws_vpc.eks_vpc.id
 }
 
 resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "eks-nat-eip"
+  }
 }
 
 resource "aws_nat_gateway" "eks_nat_gw" {
@@ -68,5 +74,3 @@ resource "aws_nat_gateway" "eks_nat_gw" {
   allocation_id = aws_eip.nat_eip.id
 }
 
-
-# ... Adicione mais recursos conforme necessário (por exemplo, Internet Gateway, NAT Gateway, etc.)
--- infra/tab_router.tf
+++ infra/tab_router.tf
@@ -35,3 +35,12 @@ resource "aws_route_table_association" "eks_private_rta_2" {
   subnet_id      = aws_subnet.eks_subnet_private_2.id
   route_table_id = aws_route_table.eks_private_rt.id
 }
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.eks_default_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
