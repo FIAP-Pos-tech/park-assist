827d3082e290b0bfb805e9a7a7ad4ada5d0f806e
Erik Hakamada <erik.hakamada@hotmail.com>
2023-11-08 21:38:26 -0300

    ajustando k8s

--- iac/cluster_eks.tf
+++ iac/cluster_eks.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "eks-cluster" {
-    name = "eks-cluster-${var.pk_cluster_name}"
-    assume_role_policy = <<POLICY
+  name               = "eks-cluster-${var.pk_cluster_name}"
+  assume_role_policy = <<POLICY
     {
         "Version": "2012-10-17",
         "Statement": [
@@ -17,27 +17,102 @@ resource "aws_iam_role" "eks-cluster" {
 }
 
 resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    role       = aws_iam_role.eks-cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
 }
 
 resource "aws_eks_cluster" "cluster" {
-    name = var.pk_cluster_name
-    version = var.pk_cluster_version
-    role_arn = aws_iam_role.eks-cluster.arn
-    
-    vpc_config {
-      endpoint_private_access = false
-      endpoint_public_access = true
-      public_access_cidrs = ["0.0.0.0/0"]
-      
-      subnet_ids = [
-          aws_subnet.private-us-east-1a.id,
-          aws_subnet.private-us-east-1b.id,
-          aws_subnet.public-us-east-1a.id,
-          aws_subnet.public-us-east-1b.id
-      ]
+  name     = var.pk_cluster_name
+  version  = var.pk_cluster_version
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
+}
+
+resource "aws_eks_node_group" "cluster_node_group" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "park-assist-node-group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  launch_template {
+    id      = aws_launch_template.park-assist-template.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "park-assist-eks-nodes"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_instance_profile" "eks_nodes" {
+  name = "eks-node-group-instance-profile"
+  role = aws_iam_role.eks_nodes.name
+}
+
+
+resource "aws_launch_template" "park-assist-template" {
+  name_prefix   = "park-assist-template"
+  image_id      = "ami-0c97930d0d19e564a"
+  instance_type = "t3.small"
+
+  monitoring {
+    enabled = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "park-assist-template"
     }
-    
-    depends_on = [ aws_iam_role_policy_attachment.amazon-eks-cluster-policy ]
+  }
+
 }
